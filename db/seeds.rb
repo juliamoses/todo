8f110630256a6a148a8ee83928e90e5a948d209a
Julia Moses <jslmoses@gmail.com>
2019-07-23 18:50:58 -0400

    db seeded with list items

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+#rails g scaffold list todos:string completed:boolean
+
+List.create!(todos: 'Go to the park')
+List.create!(todos: 'Finish a chapter of my book')
+List.create!(todos: 'Eat breakfast')
