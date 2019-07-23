29e280754fe0ed6db57831e728636dcac564d777
Julia Moses <jslmoses@gmail.com>
2019-07-23 16:09:44 -0400

    button to delete all list items at once created

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   resources :lists
+  # route that calls index method of list controller, lists now on home page
   root 'lists#index'
+  # route that calls delete_todo of delete controller, to delete marked items
+  get "delete/delete_todo"
+  # route that calls delete_all of delete controller, to delete all items
+  get "delete/delete_all"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
