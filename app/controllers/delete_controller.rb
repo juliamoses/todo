2375ac3b6f434ae72924327664ac95f68bfecedb
Julia Moses <jslmoses@gmail.com>
2019-07-23 15:59:34 -0400

    new button to delete all items from list

--- app/controllers/delete_controller.rb
+++ app/controllers/delete_controller.rb
@@ -5,4 +5,10 @@ class DeleteController < ApplicationController
 		@delete_todo = List.where(:complete => true).destroy_all
 		redirect_to lists_url, notice: 'Crossed off Todos were successfully deleted.'
 	end
+	#method in new delete controllor to delete all items in list
+	def delete_all
+		#db query to delete all items
+		@delete_all = List.all.destroy_all
+		redirect_to lists_url, notice: 'All Todos were successfully deleted.'
+	end
 end
